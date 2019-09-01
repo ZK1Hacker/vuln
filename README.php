b0fa011e16a9f1c8ee2fa8ffd886c4ca025b4e6a
ZK1Hacker <47277281+ZK1Hacker@users.noreply.github.com>
2019-09-01 17:43:26 +0700

    Add files via upload
    Mantap

---
+++ README.php
@@ -0,0 +1,15 @@
+✓pkg update
+✓pkg upgrade
+✓pkg install git
+✓pkg install php
+✓pkg install figlet
+✓pip2 install lolcat
+✓git clone https://github.com/ZK1Hacker/vuln
+✓cd vuln
+✓sh vuln.sh
+
+Jangan Lupa Subscribe Channel RoniYT
+Banyak Tutorial Tentang Hacking
+Link : https://m.youtube.com/channe✓pkg update
+
+
---
+++ vuln.sh
@@ -0,0 +1,43 @@
+clear
+figlet VULN |lolcat
+echo "\33[0;36m[==============================================\33[0;36m]"
+echo "\33[0;36m[        \33[31;1m✪Kumpulan Website VULN Deface✪        \33[0;36m]"
+echo "\33[0;36m[             \33[1;33m[$]Author : RoniYT               \33[0;36m]"
+echo "\33[0;36m[         \33[1;33m[✓] Gunakan Dengan Bijak [✓]         \33[0;36m]"
+echo "\33[0;36m[==============================================\33[0;36m]"
+echo
+
+echo "\33[33;1m✪ZK1_Cyber INDONESIAN✪"
+echo "\33[31;1m[1] \33[32;1mMelanjutkan"
+echo "\33[31;1m[2] \33[32;1mKeluar"
+echo
+echo "\33[31;1m[3] \33[32;1mInfo Tentang Saya"
+
+read -p "Masukan Nomor Pilihan :" roniyt
+
+case $roniyt in
+1)
+clear
+figlet ZK1 |lolcat
+php vuln.php
+;;
+2)
+clear
+echo "\33[31;1mBerhasil Keluar"
+echo "\33[31;1mTerimakasih Telah Menggunakan Tool Saya"
+;;
+3)
+clear
+echo "\33[32;1mHallo Nama Saya Roni"|lolcat
+echo "\33[32;1mSaya Adalah Anggota ZK1_Cyber INDONESIAN"|lolcat
+echo "\33[32;1mIngin Tutorial Lainnya ?"|lolcat
+echo "\33[32;1mCek Channel RoniYT"|lolcat
+echo "\33[32;1mLink : https://m.youtube.com/channel/UCNHZbNTS69kwCYJ8jcLzUzQ/"|lolcat
+echo "\33[1;33mSalam Hangat : Sahabat ZK1 :)"|lolcat
+;;
+*)
+echo "Maaf Sahabat,Kamu Salah Masukan Nomor"
+sleep 5
+sh vuln.sh
+;;
+esac
